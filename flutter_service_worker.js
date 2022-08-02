7aa7ef985dec685f2d8a926eb4d57d1b72195297
Lppapineau <59543166+Lppapineau@users.noreply.github.com>
2022-08-02 12:39:41 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3346eab5a92d42a8bade886eebc6d716",
 "favicon.ico": "7bc69a4195a9b7cf9ad117e515fdc0d4",
-"index.html": "5f42ef22026ea05959198ba277d2d058",
-"/": "5f42ef22026ea05959198ba277d2d058",
-"main.dart.js": "8783b3ad5a255171e5f1ed65a4586f92",
+"index.html": "c8bd8a5ad83d4908f766690864be18d4",
+"/": "c8bd8a5ad83d4908f766690864be18d4",
+"main.dart.js": "994d56cba7188feb898b09236396e839",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "644d68da75a6ad6ee84d4895b123a022",
 "icons/Icon-maskable-192.png": "644d68da75a6ad6ee84d4895b123a022",
